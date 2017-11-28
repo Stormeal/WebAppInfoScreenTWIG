05b56fbfb6b86cf829660b09fa4a6e4e5589ea9a
Nicki Kim Feldt <nick135f@edu.easj.dk>
2017-11-28 14:37:43 +0100

    Lavet view til VejrData, fået connection gennem til API i vejrData men har ikke fået det sendt afsted til view rigtigt endnu

--- controllers/VejrData.php
+++ controllers/VejrData.php
@@ -5,3 +5,31 @@
  * Date: 28-11-2017
  * Time: 11:24
  */
+
+$api = "http://api.openweathermap.org/data/2.5/weather?id=2614478&APPID=651c5fed00811436ea7544bf41e0a32c";
+
+$test = file_get_contents($api);
+$convertToAssociativeArray = true;
+$weather = json_decode($test, $convertToAssociativeArray);
+
+echo "<pre>";
+print_r($weather["main"]["temp"]);
+echo "</pre>";
+echo "<pre>";
+print_r($weather);
+echo "</pre>";
+
+$weatherStatus = $weather["weather"];
+
+
+require_once '../vendor/autoload.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('../views');
+$twig = new Twig_Environment($loader, array('auto_reload' => true));
+$template = $twig->loadTemplate('VejrDataTwig.html.twig');
+$parametersToTwig = array("weatherStatus" => $weatherStatus);
+echo $template->render($parametersToTwig);
+
+
+
+?>
