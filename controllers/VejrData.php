9c7243d831734e5638f99489e0d756377208be78
Nicki Kim Feldt <nick135f@edu.easj.dk>
2017-11-30 11:09:06 +0100

    Fixet index.php tilbage til index.html og smidt php data over i vejrdata.php

--- controllers/VejrData.php
+++ controllers/VejrData.php
@@ -4,20 +4,26 @@ $test = file_get_contents($api);
 $convertToAssociativeArray = true;
 $weather = json_decode($test, $convertToAssociativeArray);
 
-echo "<pre>";
-print_r($weather["weather"][0]);
-echo "</pre>";
+/*
 echo "<pre>";
 print_r($weather);
 echo "</pre>";
+*/
 
+$weatherTemperature = $weather["main"]["temp"];
 $weatherStatus = $weather["weather"][0]["main"];
+$weatherTownName = $weather["name"];
+$weatherTemperature -= 273.15;
+
+$weatherTotal[0] = $weatherTemperature;
+$weatherTotal[1] = $weatherStatus;
+$weatherTotal[2] = $weatherTownName;
 
 require_once '../vendor/autoload.php';
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('../views');
 $twig = new Twig_Environment($loader, array('auto_reload' => true));
 $template = $twig->loadTemplate('VejrDataTwig.html.twig');
-$parametersToTwig = array("weatherStatus" => $weatherStatus);
+$parametersToTwig = array("weatherTotal" => $weatherTotal);
 echo $template->render($parametersToTwig);
 ?>
