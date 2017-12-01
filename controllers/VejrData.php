f4c132a9c8319d7c8e8619d1e791593a9a1c3168
Nicki Kim Feldt <nick135f@edu.easj.dk>
2017-12-01 12:07:06 +0100

    opdaterer master branch til at acceptere min branch

--- controllers/VejrData.php
+++ controllers/VejrData.php
@@ -23,7 +23,7 @@ function VejrData()
 
     require_once 'vendor/autoload.php';
     Twig_Autoloader::register();
-    $loader = new Twig_Loader_Filesystem('views');
+    $loader = new Twig_Loader_Filesystem('views/Twig');
     $twig = new Twig_Environment($loader, array('auto_reload' => true));
     $template = $twig->loadTemplate('VejrDataTwig.html.twig');
     $parametersToTwig = array("weatherTotal" => $weatherTotal);
